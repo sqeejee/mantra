9847a36fc2199ce3aed6a5029fa19337ea23eb0d
sam <baconsamp@gmail.com>
2023-09-26 12:55:05 -0700

    env changes

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 require("dotenv").config();
 
-const mongoUrl =
-  "mongodb+srv://45oOjXnRNQSHQvNl:45oOjXnRNQSHQvNl@cluster0.9wcnpni.mongodb.net/?retryWrites=true&w=majority";
+const mongoUrl = process.env.DB_URL;
 const dbName = "Cluster0";
 const collectionName = "messages";
 
